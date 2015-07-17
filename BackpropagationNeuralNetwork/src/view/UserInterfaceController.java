8a3c6f50c128a6e456a48d8fa01c483f362812ec
Richard O'Brien <richard.obrien.sa@gmail.com>
2015-07-17 11:17:34 +0200

    Restructure packages

--- BackpropagationNeuralNetwork/src/view/UserInterfaceController.java
+++ BackpropagationNeuralNetwork/src/view/UserInterfaceController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package backpropagationneuralnetwork;
+package view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author Richard O'Brien
  */
-public class FXMLDocumentController implements Initializable {
+public class UserInterfaceController implements Initializable {
 	
 	@FXML
 	private Label label;
