06f35c50afd64fd514bf221a93e8e065df8fc3f6
Richard O'Brien <richard.obrien.sa@gmail.com>
2015-07-17 11:34:55 +0200

    Add ActivationFunction interface and Sigmoid implementation

---
+++ BackpropagationNeuralNetwork/src/activation/Sigmoid.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activation;
+
+/**
+ *
+ * @author Richard O'Brien
+ */
+public class Sigmoid implements ActivationFunction{
+	
+	@Override
+	public double calculateActivation (double input) {
+		double divisor = 1.0 + Math.exp(-input);
+		
+		return 1/divisor;
+	}
+}
