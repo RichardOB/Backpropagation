9b604831f889bb81c5a50f274c07df6eb410b475
Richard O'Brien <richard.obrien.sa@gmail.com>
2015-07-29 09:28:51 +0200

    Update data preparation tool

--- BackpropagationNeuralNetwork/src/utilities/DataPreparator.java
+++ BackpropagationNeuralNetwork/src/utilities/DataPreparator.java
@@ -13,6 +13,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -26,6 +28,12 @@ public class DataPreparator {
 	private TrainingSet trainingSet;
 	
 	private GeneralisationSet generalisationSet;
+	
+	private double [][] input;
+	
+	private double [][] output;
+	
+	private int inputCount;
 	
 	public void readProblemSetFromFile(String path) throws FileNotFoundException, IOException, Exception {
 		File file = new File(path);
@@ -70,16 +78,27 @@ public class DataPreparator {
 		});
 	}
 	
-	public void preprocessData() {
-		//Alter range of data
+	private void printValueArray (double[][] arr) {
+		
+		for (double[] arr1 : arr) {
+			for (int j = 0; j < arr1.length; j++) {
+				System.out.print(arr1[j]);
+				
+				if (j != arr1.length - 1) {
+					System.out.print(",");
+				}
+			}
+			System.out.println("");
+		}
 	}
+		
 	
-	public int splitDataSet(int outputCount) throws Exception {
+	public int preprocessData(int outputCount) throws Exception {
 		
-		int inputCount = unprocessedProblemSet.get(0).length - outputCount;
+		inputCount = unprocessedProblemSet.get(0).length - outputCount;
 		
-		double [][] input = new double[unprocessedProblemSet.size()][inputCount];
-		double [][] output = new double[unprocessedProblemSet.size()][outputCount];
+		input = new double[unprocessedProblemSet.size()][inputCount];
+		output = new double[unprocessedProblemSet.size()][outputCount];
 		
 		//Shuffle data
 		
@@ -95,30 +114,77 @@ public class DataPreparator {
 			int count = 0;
 			
 			for (int j = 0; j < problemInstance.length; j++) {
-				
-				//System.out.println("i: " + i);
-				//System.out.println("j: " + j);
-				//System.out.println("Count: " + count);
-				
 				if (j < inputCount) {
-					//System.out.println("\tj < inputCount");
 					input[i][j] = problemInstance[j];
-					//System.out.println("\tinput[" + i + "][" + j + "]");
 				}
 				else if (j >= inputCount) {
-					//System.out.println("\tj >= inputCount");
 					output[i][count++] = problemInstance[j];
-					//System.out.println("\toutput[" + i + "][" + (count - 1) + "]");
 				}
 				else {
 					throw new Exception("input and output count have disointability issues");
 				}
 			}
-			
-			//System.out.println("");
-	
 		}
+
+		//Alter range of inputs
+		double[][] sortedInputs = new double[input.length][input[0].length];
+		System.arraycopy(input, 0, sortedInputs, 0, input.length);
+			
+		sortedInputs = alterRange(input, -Math.sqrt(3.0), Math.sqrt(3.0));
 		
+		//Set inputs to sorted array
+		System.arraycopy(sortedInputs, 0, input, 0, sortedInputs.length);
+		
+		
+		//Alter range of outputs
+		double[][] sortedOutputs = new double[output.length][output[0].length];
+		System.arraycopy(output, 0, sortedOutputs, 0, output.length);
+		
+		sortedOutputs = alterRange(output, 0.0, 1.0);
+		
+		//Set outputs to sorted array
+		System.arraycopy(sortedOutputs, 0, output, 0, sortedOutputs.length);
+		
+		splitDataSet(outputCount);
+		return inputCount;
+	}
+	
+	private double[][] alterRange(double[][] unsortedData, double tsMin, double tsMax) {
+		
+		double[][] sortedData = new double[unsortedData.length][unsortedData[0].length];
+		System.arraycopy(unsortedData, 0, sortedData, 0, unsortedData.length);
+			
+		//For all inputs
+		for (int col = 0; col < sortedData[0].length; col ++) {
+			
+			//bubble sort according to input
+			for (int i = 0; i < sortedData.length; i++) {
+				for (int j = i+1; j < sortedData.length; j++) {
+					if (sortedData[i][col]>sortedData[j][col]) {
+						double temp = sortedData[i][col];
+						sortedData[i][col] = sortedData[j][col];
+						sortedData[j][col] = temp;
+					}
+				}
+			}
+			
+			//range of unscaled values
+			double tuMin = sortedData[0][col];
+			double tuMax = sortedData[sortedData.length - 1][col];
+			
+			//calculate new scaled values
+			for (double[] sortedInput : sortedData) {
+				double tu = sortedInput[col];
+				double ts = ((tu - tuMin)/(tuMax - tuMin)) * (tsMax - tsMin) + tsMin;
+				sortedInput[col] = ts;
+			}
+        }
+		
+		return sortedData;
+	}
+	
+	public int splitDataSet(int outputCount) throws Exception {
+
 		int trainingDataCount = (int) Math.ceil(unprocessedProblemSet.size() * 0.8);
 		int generalisationDataCount = (int) Math.floor(unprocessedProblemSet.size() * 0.2);
 		
