193292a5b070d0f9865c71ac73fe4ddcb04c6b45
Richard O'Brien <richard.obrien.sa@gmail.com>
2015-08-22 13:40:32 +0200

    Add hyperbolic tangent and linear activation functions

---
+++ BackpropagationNeuralNetwork/src/activation/HyperbolicTangent.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activation;
+
+/**
+ *
+ * @author Richard O'Brien
+ */
+public class HyperbolicTangent implements ActivationFunction{
+	
+	@Override
+	public double calculateActivation (double input) {
+		
+		double dividend = Math.exp(input) - Math.exp(-input);
+		double divisor = Math.exp(input) + Math.exp(-input);
+		
+		return dividend/divisor;
+	}
+	
+}
